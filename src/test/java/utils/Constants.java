1d37fb2bc1ab3a312b3d5b67e8a38a52599054f3
Bogdanov-Tsabert Timogor <mr.timogor@mail.ru>
2025-02-26 22:29:11 +0500

    Made client class, also made urls constants

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+    public static final String INGREDIENTS_URL = "https://stellarburgers.nomoreparties.site/api/ingredients";
+    public static final String ORDERS_URL = "https://stellarburgers.nomoreparties.site/api/orders";
+    public static final String REGISTER_URL = "https://stellarburgers.nomoreparties.site/api/auth/register";
+    public static final String AUTHORIZATION_URL = "https://stellarburgers.nomoreparties.site/api/auth/login";
+    public static final String USER_INFO_URL = "https://stellarburgers.nomoreparties.site/api/auth/user";
+    public static final String ALL_ORDERS_URL = "https://stellarburgers.nomoreparties.site/api/orders/all";
+    public static final String LOGOUT_URL = "https://stellarburgers.nomoreparties.site/api/auth/logout";
+}
---
+++ src/test/java/utils/StellarBurgerClient.java
@@ -0,0 +1,106 @@
+package utils;
+
+import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.response.ValidatableResponse;
+import model.OrderToSend;
+import model.RefreshToken;
+import model.UserToSend;
+
+import static io.restassured.RestAssured.given;
+import static utils.Constants.*;
+
+public class StellarBurgerClient {
+
+    @Step("Клиент -- получение списка ингредиентов")
+    public ValidatableResponse getIngredients(){
+        return given()
+                .filter(new AllureRestAssured())
+                .get(INGREDIENTS_URL)
+                .then();
+    }
+
+    @Step("Клиент -- создание заказа")
+    public ValidatableResponse createOrder(OrderToSend order, String authToken){
+        return given()
+                .filter(new AllureRestAssured())
+                .header("Authorization", authToken)
+                .body(order)
+                .post(ORDERS_URL)
+                .then();
+    }
+
+    //public ValidatableResponse restorePassword(){}
+
+    @Step("Клиент -- создание пользователя")
+    public ValidatableResponse createUser(UserToSend user){
+        return given()
+                .filter(new AllureRestAssured())
+                .body(user)
+                .post(REGISTER_URL)
+                .then();
+    }
+
+    @Step("Клиент -- авторизация пользователя")
+    public ValidatableResponse authorizeUser(UserToSend user){
+        return given()
+                .filter(new AllureRestAssured())
+                .body(user)
+                .post(AUTHORIZATION_URL)
+                .then();
+    }
+
+    public ValidatableResponse quitUser(RefreshToken refreshToken){
+        return given()
+                .filter(new AllureRestAssured())
+                .body(refreshToken)
+                .post(LOGOUT_URL)
+                .then();
+    }
+
+    //public ValidatableResponse refreshUserToken(){}
+
+    @Step("Клиент -- получение информации о пользователе")
+    public ValidatableResponse getUserInfo(String authToken){
+        return given()
+                .filter(new AllureRestAssured())
+                .header("Authorization", authToken)
+                .get(USER_INFO_URL)
+                .then();
+    }
+
+    @Step("Клиент -- обновление информации о пользователе")
+    public ValidatableResponse updateUserInfo(String authToken, UserToSend user){
+        return given()
+                .filter(new AllureRestAssured())
+                .header("Authorization", authToken)
+                .body(user)
+                .patch(USER_INFO_URL)
+                .then();
+    }
+
+    @Step("Клиент -- удаление пользователя")
+    public ValidatableResponse deleteUser(String authToken){
+        return given()
+                .header("Authorization", authToken)
+                .delete(USER_INFO_URL)
+                .then();
+    }
+
+    @Step
+    public ValidatableResponse getAllOrders(){
+        return given()
+                .filter(new AllureRestAssured())
+                .get(ALL_ORDERS_URL)
+                .then();
+    }
+
+    public ValidatableResponse getOrdersByUser(String authToken){
+        return given()
+                .filter(new AllureRestAssured())
+                .header("Authorization", authToken)
+                .get(ORDERS_URL)
+                .then();
+    }
+
+}
